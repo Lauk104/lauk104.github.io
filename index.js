50945e80e23fd5b40d30a3c1696a0e51aa67606f
Jason Lau <lauk104@newschool.edu>
2016-12-14 03:57:27 +0000

    xd

--- index.js
+++ index.js
@@ -24,12 +24,17 @@ setTimeout(function () {
          $('#circle3').css("width","8");
          $('#circle3').css("height","8");
           }, 200);
+          
+          setTimeout(function () {
+         $('#circle4').css("width","8");
+         $('#circle4').css("height","8");
+          }, 300);
 }, 1200);
 console.log(currPage);
 $('body').bind('mousewheel', function(e){
 
 
-     if(e.originalEvent.deltaY > 80) {
+     if(e.originalEvent.deltaY > 75) {
          //scroll down
          
          if (currPage==0){
@@ -140,6 +145,70 @@ $('body').bind('mousewheel', function(e){
          $('#rectangle2').css("height","37.5");
          
          $('visit2').css("opacity","1");
+         
+         canScroll=true;
+         }, 600);
+         console.log(currPage);
+         }
+         
+          console.log("scrolldn");
+          
+           console.log(e.originalEvent);
+           
+         }
+         
+         if (currPage==2){
+             if(canScroll){
+              canScroll=false;
+          $('h5').css("opacity","0");
+         $('h5').css("top","-50");
+         
+         $('h51').css("opacity","0");
+         $('h51').css("top","-50");
+         
+         $('h52').css("opacity","0");
+         $('h52').css("top","150");
+         
+         $('#line').css("width","0");
+         
+         $('#rectangle2').css("height","0");
+         
+         $('visit2').css("opacity","0");
+         
+         $('#circle3').css("width","8");
+         $('#circle3').css("height","8");
+         $('#circle3').css("opacity",".3");
+         
+         $('h142').css("opacity","0");
+         $('h142').css("top","150");
+         
+         setTimeout(function () {
+         
+         $('h152').css("opacity",".15");
+         $('h152').css("top","200");
+         
+         $('#circle4').css("width","15");
+         $('#circle4').css("height","15");
+         $('#circle4').css("opacity","1");  
+         }, 200);
+         
+         currPage=3;
+         
+         setTimeout(function () {
+         $('h6').css("opacity","1");
+         $('h6').css("top","0");
+         
+         $('h61').css("opacity","1");
+         $('h61').css("top","0");
+         
+         $('h62').css("opacity","1");
+         $('h62').css("top","200");
+         
+         $('#line').css("width","47.5");
+         
+         $('#rectangle2').css("height","37.5");
+         
+         $('visit3').css("opacity","1");
          
          canScroll=true;
          }, 600);
@@ -269,6 +338,71 @@ $('body').bind('mousewheel', function(e){
          $('#rectangle2').css("height","37.5");
          
          $('visit').css("opacity","1");
+         
+         canScroll=true;
+           }, 600);
+            
+            
+          }
+          
+          
+          
+     }
+     
+     if(currPage==3)
+          {
+              if(canScroll){
+           canScroll=false;
+             
+             
+             $('h6').css("opacity","0");
+         $('h6').css("top","50");
+         
+         $('h61').css("opacity","0");
+         $('h61').css("top","50");
+         
+         $('h62').css("opacity","0");
+         $('h62').css("top","250");
+         
+         $('#line').css("width","0");
+         
+         $('#rectangle2').css("height","0");
+         
+         $('visit3').css("opacity","0");
+         
+         $('#circle4').css("width","8");
+         $('#circle4').css("height","8");
+         $('#circle4').css("opacity",".3");
+         
+         $('h152').css("opacity","0");
+         $('h152').css("top","250");
+         
+         setTimeout(function () {
+         
+         $('h142').css("opacity",".15");
+         $('h142').css("top","200");
+         
+         $('#circle3').css("width","15");
+         $('#circle3').css("height","15");
+         $('#circle3').css("opacity","1");  
+         }, 200);
+         
+            currPage=2; 
+           setTimeout(function () {
+             $('h5').css("opacity","1");
+         $('h5').css("top","0");
+         
+         $('h51').css("opacity","1");
+         $('h51').css("top","0");
+         
+         $('h52').css("opacity","1");
+         $('h52').css("top","200");
+         
+         $('#line').css("width","47.5");
+         
+         $('#rectangle2').css("height","37.5");
+         
+         $('visit2').css("opacity","1");
          
          canScroll=true;
            }, 600);
